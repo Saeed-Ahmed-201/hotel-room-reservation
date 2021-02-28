4b474c62de339d55add68e6964a1bf70d69ed7b4
Saeed Ahmed Sheikh <saeedsheikhdadu@gmail.com>
2021-03-01 00:40:50 +0500

    error correction

--- src/main/java/com/hotel/HotelApplication.java
+++ src/main/java/com/hotel/HotelApplication.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.hotel"})
-publ  HotelApplication {
+public class   HotelApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HotelApplication.class, args);
